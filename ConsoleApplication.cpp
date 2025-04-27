b6d2392d4182152f2ef7803a159a05e9886a9dd3
ethnwr <daniil.gawrilov07@gmail.com>
2025-04-28 02:06:10 +0900

    Create ConsoleApplication.cpp

---
+++ ConsoleApplication.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include "Set.h"
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    Set p;
+    Set b;
+    b.completion();
+    p - b;
+    
+    
+    
+    
+}
